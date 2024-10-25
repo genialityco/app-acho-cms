388fd2f4de2dead2e75dcff8fe433ab943312d77
balderban <balderban@gmail.com>
2024-10-25 10:51:34 -0500

    agregando versión inicial de creación de speakers

--- src/pages/speakers/create.tsx
+++ src/pages/speakers/create.tsx
@@ -1,18 +1,64 @@
-import { useState } from 'react';
 import { Create, useForm, useSelect } from "@refinedev/mantine";
-import { Select, TextInput, Text, Badge, Group } from "@mantine/core";
+import {
+  Select,
+  Button,
+  Group,
+  TextInput,
+  Text,
+  Stack,
+  MultiSelect,
+  Badge,
+  Textarea,
+  Switch,
+  useMantineTheme,
+} from "@mantine/core";
 import MDEditor from "@uiw/react-md-editor";
 import ArrayTagInput from "./arrayTagInput";
+import type { ICategory } from "../../interfaces";
+import { DateField } from "@refinedev/mantine";
+import { DatePicker } from "@mantine/dates";
+import { v4 as uuidv4 } from "uuid";
+import dayjs from "dayjs";
+import { useState } from "react";
+import { Dropzone, DropzoneProps, IMAGE_MIME_TYPE } from "@mantine/dropzone";
+import { IconUpload, IconPhoto, IconX } from "@tabler/icons-react";
+import axios from "axios";
+
+
+// const handleFormSubmit = (updatedSessions) => {
+//   console.log("Updated Sessions:", updatedSessions);
+// };
+
 export const SpeakerCreate: React.FC = () => {
-  const { saveButtonProps, values, getInputProps, errors } = useForm({
+  const [files, setFiles] = useState<File[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const {
+    saveButtonProps,
+    getInputProps,
+    values,
+    setFieldValue,
+    refineCore: { query: queryResult },
+    errors,
+  } = useForm({
+    refineCoreProps: {
+      redirect: false,
+    },
     initialValues: {
-      "eventId":"66f1e0b57c2e2fbdefa21271",
-      "title": "Investigacion Geniality"+(new Date().toString()),
-      "category": "Science",
-      "topic": "Quantum Physics",
-      "institution": "MIT",
-      "urlPdf":"https://firebasestorage.googleapis.com/v0/b/global-auth-49737.appspot.com/o/certificado_ejemplo-1.pdf?alt=media&token=1aa2097a-d152-469c-b3cd-66dc386138d1",
-      authors:[]
+      names: "NN",
+      description: "Good Speaker",
+      location: "Planet Earth",
+      isInternational: false,
+      imageUrl: "",
+      eventId: "66f1e0b57c2e2fbdefa21271",
+    },
+    transformValues: (values) => {
+      return values;
+      // let respuesta =       {
+      //   ...values,
+      //   eventId: values?.eventId?._id?values?.eventId?._id:eventId
+      // }
+      // return respuesta
     },
     // validate: {
     //   title: (value) => (value.length < 2 ? "Too short title" : null),
@@ -24,73 +70,171 @@ export const SpeakerCreate: React.FC = () => {
     // },
   });
 
+  const [inputValue, setInputValue] = useState("");
 
-  const [inputValue, setInputValue] = useState('');
+  const theme = useMantineTheme();
+
+  // Handle file upload
+  const handleFileUpload = async () => {
+    if (files.length === 0) {
+      alert("No files selected!");
+      return;
+    }
+
+    const formData = new FormData();
+    files.forEach((file) => {
+      formData.append("file", file); // Append the file to FormData
+    });
+
+    try {
+      setLoading(true);
+      const response = await axios.post("https://lobster-app-uy9hx.ondigitalocean.app/upload/image", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log("yupi subio", response.data.imageUrl);
+      getInputProps("imageUrl").onChange(response.data.imageUrl);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      alert("Failed to upload the file.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const handleKeyDown = (event) => {
-    if (event.key === 'Enter' && inputValue.trim()) {
+    if (event.key === "Enter" && inputValue.trim()) {
       const newValue = [...values.authors, inputValue.trim()];
-      getInputProps(`authors`).onChange(newValue) 
-      setInputValue('');
-
+      getInputProps(`authors`).onChange(newValue);
+      setInputValue("");
     }
   };
   const removeTag = (tagToRemove) => {
-    const newValue = (values.authors.filter((tag) => tag !== tagToRemove));
+    const newValue = values.authors.filter((tag) => tag !== tagToRemove);
     getInputProps(`authors`).onChange(newValue);
   };
 
-  // const { selectProps } = useSelect({
+  //console.log('getInputProps,',  getInputProps("title"))
+
+  // const { selectProps } = useSelect<ICategory>({
   //   resource: "categories",
+  //   defaultValue: queryResult?.data?.data.category.id,
   // });
 
   return (
     <Create saveButtonProps={saveButtonProps}>
-    <form >
-    <TextInput mt={8} label="eventId" placeholder="eventId" {...getInputProps("eventId")} />
-    <TextInput mt={8} label="title" placeholder="title" {...getInputProps("title")} />
-    <TextInput mt={8} label="topic" placeholder="topic" {...getInputProps("topic")} />
-
-    <TextInput mt={8} label="institution" placeholder="institution" {...getInputProps("institution")} />
-    <Select
-        mt={8}
-        label="Category"
-        placeholder="Pick one"
-        {...getInputProps("category")}
-        data={[
-          { label: "Hematología", value: "Hematología" },
-          { label: "Oncología", value: "Oncología" },
-        ]}
-      />
-      <TextInput mt={8} label="urlPdf" placeholder="urlPdf" {...getInputProps("urlPdf")} />
-      <div>
-
-      <div>
-      <Text>Autores</Text>
-      <Group spacing="xs">
-        {values.authors.map((tag, index) => (
-          <Badge
-            key={index}
-            variant="filled"
-            color="blue"
-            rightSection={<span onClick={() => removeTag(tag)} style={{ cursor: 'pointer', marginLeft: 4 }}>x</span>}
-          >
-            {tag}
-          </Badge>
-        ))}
+      <form>
         <TextInput
-          placeholder="Type and press Enter"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.currentTarget.value)}
-          onKeyDown={handleKeyDown}
-          sx={{ flexGrow: 1 }}
+          mt={8}
+          label="eventId"
+          placeholder="66f1e0b57c2e2fbdefa21271"
+          {...getInputProps("eventId")}
+          disabled
         />
-      </Group>
-      </div>
-    </div>
+        <TextInput mt={8} label="Nombre Completo" placeholder="names" {...getInputProps("names")} />
+        <Textarea mt={8} label="Descripción" placeholder="description" {...getInputProps("description")} />
 
+        <TextInput mt={8} label="Pais/Ubicación" placeholder="location" {...getInputProps("location")} />
+        <p></p>
+        <Switch label="isInternational" placeholder="isInternational" {...getInputProps("isInternational")} />
 
-     </form>
+        <TextInput mt={8} label="imageUrl" placeholder="imageUrl" {...getInputProps("imageUrl")} />
+
+        {values.imageUrl && <img style={{ width: "200px" }} src={values.imageUrl} />}
+
+        <Dropzone
+          onDrop={(files) => {
+            console.log("accepted files", files);
+            setFiles(files);
+          }} 
+          onReject={(files) => console.log("rejected files", files)}
+          maxSize={3 * 1024 ** 2}
+          accept={IMAGE_MIME_TYPE}
+        >
+          <Group position="center" spacing="xl" style={{ minHeight: "rem(220)", pointerEvents: "none" }}>
+            <Dropzone.Accept>
+              <IconUpload
+                size="3.2rem"
+                stroke={1.5}
+                color={theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 4 : 6]}
+              />
+            </Dropzone.Accept>
+            <Dropzone.Reject>
+              <IconX size="3.2rem" stroke={1.5} color={theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]} />
+            </Dropzone.Reject>
+            <Dropzone.Idle>
+              <IconPhoto size="3.2rem" stroke={1.5} />
+            </Dropzone.Idle>
+
+            <div>
+              {/* Display Selected Files */}
+              {files.length > 0 && (
+                <div style={{ marginTop: 10, marginBottom: 10 }}>
+                  <Text size="l" color="dimmed">
+                    Archivo seleccionado para subir:
+                  </Text>
+                  <Text size="xl">{files.map((file) => file.name).join(", ")} </Text>
+                </div>
+              )}
+
+              {files.length <= 0 && (
+                <>
+                  <Text size="l">Arrastra o haz click para cargar el archivo</Text>
+                  <Text size="sm" color="dimmed" inline mt={7}>
+                    El archivo no debe exceder los 10MB
+                  </Text>
+                </>
+              )}
+            </div>
+          </Group>
+        </Dropzone>
+
+        <Button disabled={!(files.length > 0)} style={{ width: "100%" }} onClick={handleFileUpload} loading={loading}>
+          Upload
+        </Button>
+
+        {/*
+   
+      <Select
+          mt={8}
+          label="Category"
+          placeholder="Pick one"
+          {...getInputProps("category")}
+          data={[
+            { label: "Hematología", value: "Hematología" },
+            { label: "Oncología", value: "Oncología" },
+          ]}
+        />
+        <TextInput mt={8} label="urlPdf" placeholder="urlPdf" {...getInputProps("urlPdf")} />
+
+    */}
+
+        {/*}
+        <div>
+        <Text>Autores</Text>
+        <Group spacing="xs">
+          {values.authors.map((tag, index) => (
+            <Badge
+              key={index}
+              variant="filled"
+              color="blue"
+              rightSection={<span onClick={() => removeTag(tag)} style={{ cursor: 'pointer', marginLeft: 4 }}>x</span>}
+            >
+              {tag}
+            </Badge>
+          ))}
+          <TextInput
+            placeholder="Type and press Enter"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.currentTarget.value)}
+            onKeyDown={handleKeyDown}
+            sx={{ flexGrow: 1 }}
+          />
+        </Group>
+        </div>
+        */}
+      </form>
     </Create>
   );
 };
