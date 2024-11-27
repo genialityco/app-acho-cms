6b2e7e4246cb7433679a5e6cc3af5c309fff5e94
juanfe <lassojuanfe@gmail.com>
2024-11-27 17:29:18 -0500

    Ajusta certificates list

--- src/App.tsx
+++ src/App.tsx
@@ -184,7 +184,7 @@ const App: React.FC = () => {
               },
               {
                 name: "certificates",
-                list: "/certicates",
+                list: "/certificates",
               },
             ]}
             options={{
